1012f38b6d0984acd5581a1cfd312b648ff454f6
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2024-01-21 17:55:28 +0800

    Maybe wrote correct tests for LinksFromHTML

--- crawl.js
+++ crawl.js
@@ -1,9 +1,18 @@
 const nodeUrl = require('node:url')
+const JSDOM = require('jsdom')
 
 const normalizeURL = (url) => {
     return nodeUrl.parse(url).hostname
 }
 
+const getUrlsFromHTLML = (htmlBody, baseURL) => {
+    page = new JSDOM(htmlBody)
+    links = page.dom.window.document.querySelectorAll('a')
+    links.array.forEach(link => {
+        console.log(link)
+    });
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL, getLinks
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const {test, expect} = require('@jest/globals')
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getUrlsFromHTLML} = require('./crawl.js')
 
 test('normalizeURLWithProtocol', () => {
     expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev")
@@ -12,3 +12,8 @@ test('normalizeURLWithProtocol', () => {
 // test('normalizeURL-NoProtocol', () => {
 //    expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev") 
 // });
+
+test('LinksFromHTML', () => {
+    expect(getUrlsFromHTLML("<h1>This is a Test Page with no Links</h1>", "./testHTMLOne")).toBe(null)
+    expect(getUrlsFromHTLML('<h1>This is a Test Page with 2 Links</h1><ahref="https://www.google.com">Google</a><ahref="https://www.facebook.com">Facebook</a>')).toBe(['https://www.google.com', 'https://www.facebook.com'])
+});
